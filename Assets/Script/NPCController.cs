1764c05bc0de2dc203531f5bf321ead39ff475dd
yudaihyon-lab <hyon_music@yahoo.co.jp>
2021-05-28 08:51:22 +0900

    会話システムの修正（会話中非動作）

--- Assets/Script/NPCController.cs
+++ Assets/Script/NPCController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Fungus;
@@ -6,28 +7,33 @@ using Fungus;
 [RequireComponent(typeof(Flowchart))]
 public class NPCController : MonoBehaviour
 {
-	[SerializeField]
-	string message = "";
-	GameObject playerObj;
-	Flowchart flowChart;
+    [SerializeField]
+    string message = "";
+    GameObject playerObj;
+    Flowchart flowChart;
+    player player;
 
-    // Start is called before the first frame update
     void Start()
     {
-		playerObj = GameObject.FindGameObjectWithTag("Player");
-		flowChart = GetComponent<Flowchart>();        
+        playerObj = GameObject.FindGameObjectWithTag("Player");
+        player = playerObj.GetComponent<player>();
+        flowChart = GetComponent<Flowchart>();
     }
-
-    private void OnCollisionEnter2D(UnityEngine.Collision2D other)
+    private  void OnCollisionEnter2D(UnityEngine.Collision2D other)
     {
-    	if (other.gameObject.tag == "Player")
-    	{
-    		StartCoroutine(Talk());
-    	}
+        if (other.gameObject.tag == "Player")
+        {
+            StartCoroutine(Talk());
+        }
     }
+   
     IEnumerator Talk()
     {
-    	flowChart.SendFungusMessage(message);
-    	yield return new WaitUntil(() => flowChart.GetExecutingBlocks().Count == 0);
+        player.SetState(player.State.Talk);
+
+        flowChart.SendFungusMessage(message);
+        yield return new WaitUntil(() => flowChart.GetExecutingBlocks().Count == 0);
+
+        //player.SetState(player.State.Normal);
     }
 }
---
+++ Assets/Script/player.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+	public float speed;
+    // Start is called before the first frame update
+	private Animator anim = null;
+	private Rigidbody2D rb = null;
+    private Vector2 input;
+    public float stopf = 1;
+    State state;
+
+    public enum State
+    {
+        Normal,
+        Talk,
+        Command,
+    }
+
+    public void SetState(State state)
+    {
+        this.state = state;
+
+        if (state == State.Normal)
+        {
+            stopf = 1;
+            anim.enabled = true;
+        }
+        else if (state == State.Talk)
+        {
+            stopf = 0;
+            anim.enabled = false;
+        }
+        else if (state == State.Command)
+        {
+            stopf = 0;
+            anim.enabled = false;
+        }
+    }
+    
+    public State GetState()
+    {
+        return state;
+    }
+
+ 
+	void Start()
+	{
+		anim = GetComponent<Animator>();
+		this.rb = GetComponent<Rigidbody2D>();
+		this.rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+	}
+	 
+	private void Update()
+    {
+        if(stopf == 1){
+            // 入力を取得
+            input = new Vector2(
+                Input.GetAxis("Horizontal"),
+                Input.GetAxis("Vertical"));
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (input == Vector2.zero)
+        {
+            return;
+        }
+        if (stopf == 1)
+        {
+            // 移動量を加算する
+            rb.position += input * speed;
+        }
+    }
+}
